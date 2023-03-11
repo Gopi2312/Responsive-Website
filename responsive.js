02d7a844b8828ba802c64bad6052d3f0d46f9c7e
Gopi2312 <123471487+Gopi2312@users.noreply.github.com>
2023-03-11 17:35:59 +0530

    Add files via upload

---
+++ responsive.js
@@ -0,0 +1,41 @@
+let arr = [
+    {image:"images/shoe1.webp" , alt:"Abdur-Image",desc:"ABDUR RAHMAN"},
+    {image:"images/shoe2.jpeg" , alt:"Ranjith-Image",desc:"RANJITH"},
+    {image:"images/shoe3.webp" , alt:"Kl-Image",desc:"ARAVINDHAN"},
+    {image:"images/shoe4.webp" , alt:"Arun-Image",desc:"ARUN"},
+    {image:"images/shoe5.webp" , alt:"Arunachalam-Image",desc:"ARUNACHALAM"},
+    {image:"images/shoe6.jpeg" , alt:"Bhaargavi-Image",desc:"BHAARGAVI"},
+    {image:"images/shoe7.jpeg" , alt:"Bhuvaneshwari-Image",desc:"BHUVANESHWARI"},
+    {image:"images/shoe8.jpeg" , alt:"Fasila-Image",desc:"FASILA FIRTHOUS"},
+    {image:"images/shoe9.jpeg" , alt:"Ganesh-Image",desc:"GANESH"},
+    {image:"images/shoe10.jpeg" , alt:"Gopi-Image",desc:"GOPI"},
+];
+
+const render =()=>{
+    let show = "";
+    for(let i =0;i<arr.length;i++){
+       show += "<div class='image'><img src='"+arr[i].image+"' alt="+arr[i].alt+" onclick='display("+i+")'><h3>"+arr[i].desc+"</h3></div>";
+    }
+    document.getElementById("shoe-image").innerHTML=show;
+}
+
+const display = (id)=>{
+    let displayImage = "<div class = 'display-image'><img src='"+arr[id].image+"' alt="+arr[id].alt+"><p class ='cross' onclick='cancel()'>❌</p></div>";
+    document.getElementById("image-container").style.display = "flex";
+    document.getElementById("image-container").innerHTML = displayImage;
+}
+
+const cancel = ()=>{
+    document.getElementById("image-container").style.display = "none";
+}
+
+const menulist = ()=>{
+    if(document.getElementById("nav-bar-menu").style.display=="flex"){
+        document.getElementById("nav-bar-menu").style.display="none";
+        
+    }
+    else{
+        document.getElementById("nav-bar-menu").style.display="flex";
+        document.getElementById("nav-bar-menu").style.flexDirection = "column";
+    }
+}
